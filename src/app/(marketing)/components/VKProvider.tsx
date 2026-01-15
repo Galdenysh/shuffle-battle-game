800659927746bc06f6cea50aec3f3dddb0bc7dd6
Igor Petrov <Galdenysh@gmail.com>
2026-01-16 02:32:12 +0300

    feat: add VKProvider

---
+++ src/app/(marketing)/components/VKProvider.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { useEffect } from 'react';
+import type { FC } from 'react';
+import bridge from '@vkontakte/vk-bridge';
+
+const VKProvider: FC = () => {
+  useEffect(() => {
+    bridge.send('VKWebAppInit');
+  }, []);
+
+  return null;
+};
+
+VKProvider.displayName = 'VKProvider';
+
+export default VKProvider;
+
--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,9 +1,13 @@
 import { ReactNode } from 'react';
+import { VKProvider } from './components';
 
 export default function MarketingLayout({ children }: { children: ReactNode }) {
   return (
     <>
-      <main className="h-dvh w-full overflow-y-auto overflow-x-hidden">{children}</main>
+      <VKProvider />
+      <main className="h-dvh w-full overflow-y-auto overflow-x-hidden">
+        {children}
+      </main>
     </>
   );
 }
