d22d9432861c50dda9f14cc9add800bcbfa5f63c
Igor Petrov <Galdenysh@gmail.com>
2026-01-15 14:34:12 +0300

    fix: change GameInfo position

--- src/app/game/components/GameHUD.tsx
+++ src/app/game/components/GameHUD.tsx
@@ -33,7 +33,7 @@ interface GameHUDProps {
 }
 
 const containerClasses = {
-  base: 'absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-100 flex flex-col justify-between p-2 outline outline-2 outline-purple-500/30 outline-offset-2 transition-opacity duration-1200',
+  base: 'absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-100 flex flex-col justify-between p-2 outline outline-2 outline-purple-500/30 -outline-offset-2 transition-opacity duration-1200',
   visible: 'opacity-100 pointer-events-auto',
   hidden: 'opacity-0 pointer-events-none',
 } as const;
